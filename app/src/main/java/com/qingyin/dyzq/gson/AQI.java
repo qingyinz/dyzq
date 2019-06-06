a27e1bef4f30a769d25a640338f78649ecbddea6
青冘君 <153705849@qq.com>
2019-06-06 22:28:55 +0800

    原版

---
+++ app/src/main/java/com/qingyin/dyzq/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.qingyin.dyzq.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
